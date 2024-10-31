f3e5fb7c0311b1e7aec7b68516d4b03c8a73451c
beeenne <158477502+beeenne@users.noreply.github.com>
2024-10-31 01:31:41 +0100

    issue#11

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component, computed, Signal } from '@angular/core'
+import { ChangeDetectionStrategy, Component, WritableSignal, signal } from '@angular/core'
 import { MatIconButton } from '@angular/material/button'
 import { MatIcon } from '@angular/material/icon'
 import { Store } from '@ngrx/store'
+import { combineLatest } from 'rxjs'
 import { RootState } from 'src/store/app.store'
 import { TransitLinesActions } from 'src/store/transit-lines/transit-lines.actions'
 import { fromTransitLines } from 'src/store/transit-lines/transit-lines.selectors'
 import { TransitStop } from 'src/types/line'
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-detail',
@@ -17,32 +19,52 @@ import { TransitStop } from 'src/types/line'
   imports: [MatIconButton, MatIcon, CommonModule],
 })
 export class DetailComponent {
-  readonly stopName: Signal<string>
-  readonly selectedStop: Signal<TransitStop>
-  private averages: Record<string, number>
+  readonly selectedStop: WritableSignal<TransitStop> = signal<TransitStop | null>(null);
+  private averages: Record<string, number> = {
+    peopleOnAverage: 0,
+    peopleOffAverage: 0,
+    reachablePopulationWalkAverage: 0,
+    reachablePopulationBikeAverage: 0,
+    count: 0
+  }
 
   constructor(private store: Store<RootState>) {
-    this.selectedStop = this.store.selectSignal(fromTransitLines.selectedStop)
-    this.stopName = computed(() => this.selectedStop()?.name || 'No selection')
-    const allStops  = this.store.selectSignal(fromTransitLines.allStops)
-    this.averages = allStops().reduce((acc, obj) => {
-      acc.count += 1
-      acc.peopleOnAverage += Math.floor(obj.peopleOn / acc.count)
-      acc.peopleOffAverage += Math.floor(obj.peopleOff / acc.count)
-      acc.reachablePopulationWalkAverage += Math.floor(obj.reachablePopulationWalk / acc.count)
-      acc.reachablePopulationBikeAverage += Math.floor(obj.reachablePopulationBike / acc.count)
-      return acc
-    }, { peopleOnAverage: 0, peopleOffAverage: 0, reachablePopulationWalkAverage: 0, reachablePopulationBikeAverage: 0, count: 0 })
+    const allStops$ = toObservable(this.store.selectSignal(fromTransitLines.allStops));
+    const selectedStop$ = toObservable(this.store.selectSignal(fromTransitLines.selectedStop));
+
+  combineLatest([allStops$, selectedStop$])
+    .pipe()
+    .subscribe(([allStops, selectedStop]) => {
+
+      this.averages = allStops?.reduce((acc, obj) => {
+        acc.count += 1
+        acc.peopleOnAverage += Math.floor(obj.peopleOn / acc.count)
+        acc.peopleOffAverage += Math.floor(obj.peopleOff / acc.count)
+        acc.reachablePopulationWalkAverage += Math.floor(obj.reachablePopulationWalk / acc.count)
+        acc.reachablePopulationBikeAverage += Math.floor(obj.reachablePopulationBike / acc.count)
+        return acc
+      }, { peopleOnAverage: 0, peopleOffAverage: 0, reachablePopulationWalkAverage: 0, reachablePopulationBikeAverage: 0, count: 0 })
+      
+      if (selectedStop) {
+        this.selectedStop.set(selectedStop);
+      } else {
+        const storageStopId = localStorage.getItem('selectedStop');
+        
+        const storageStop = allStops.find((stop) => stop.id === storageStopId);
+        this.selectedStop.set(storageStop || null);
+      }
+    });
   }
 
   getColor(value: number, key: string) {
-    if (value > this.averages[key + 'Average']) {
+    if (this.averages[key + 'Average'] && value > this.averages[key + 'Average']) {
       return {color: 'green'}
     } 
     return {color: 'red'}
   }
 
   clearSelection(): void {
+    localStorage.setItem('selectedStop', null);
     this.store.dispatch(TransitLinesActions.SelectStop({ selectedStopId: null }))
   }
 }
